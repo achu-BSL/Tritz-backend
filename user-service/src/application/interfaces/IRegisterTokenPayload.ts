f21e54500b457e3af9a278875e2289e9a933751b
achu-BSL <asharudeenalikt@gmail.com>
2024-07-10 15:19:47 +0530

    add token managers

---
+++ user-service/src/application/interfaces/IRegisterTokenPayload.ts
@@ -0,0 +1,5 @@
+export interface IRegisterTokenPayload {
+  username: string;
+  email: string;
+  password: string;
+}
---
+++ user-service/src/infrastructure/security/RegisterTokenManager.ts
@@ -0,0 +1,16 @@
+import { IRegisterTokenPayload } from "../../application/interfaces/IRegisterTokenPayload";
+import { ITokenManager } from "../../application/interfaces/ITokenManager";
+import jwt from "jsonwebtoken";
+
+export class RegisterTokenManager
+  implements ITokenManager<IRegisterTokenPayload>
+{
+  private readonly SECRET = process.env.REGISTER_TOKEN_SECRET!;
+  generate(payload: IRegisterTokenPayload, expiry: string) {
+    return jwt.sign(payload, this.SECRET, { expiresIn: expiry });
+  }
+
+  decode(token: string) {
+    return jwt.verify(token, this.SECRET) as IRegisterTokenPayload;
+  }
+}
