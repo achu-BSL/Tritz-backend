c0053a0c650e1cbf82571c337cf90fdd0cd91054
achu-BSL <asharudeenalikt@gmail.com>
2024-07-10 16:17:50 +0530

    [update] user repository

--- user-service/src/infrastructure/repositories/UserRepository.ts
+++ user-service/src/infrastructure/repositories/UserRepository.ts
@@ -1,25 +1,66 @@
 import { IUser } from "../../application/interfaces/IUserEntity";
 import { IUserRepository } from "../../application/interfaces/IUserRepository";
+import { User } from "../../domain/entities/UserEntity";
+
+import userModel from "../database/model/UserModel";
 
 export class UserRepository implements IUserRepository {
   async save(user: IUser) {
-    //todo save user in database
-    return user;
+    const mongoUser = new userModel({
+      username: user.username,
+      email: user.email,
+      password: user.password,
+    });
+    await mongoUser.save();
+    return new User({
+      username: mongoUser.username,
+      userId: mongoUser._id.toString(),
+      email: mongoUser.email,
+      password: "",
+    });
   }
 
-  async findUserById(_userId: string) {
-    return null;
+  async findUserById(userId: string) {
+    const mongoUser = await userModel.findById(userId);
+    if (!mongoUser) return null;
+    return new User({
+      userId: mongoUser._id.toString(),
+      username: mongoUser.username,
+      email: mongoUser.email,
+      password: "",
+    });
   }
 
-  async findUserByUsername(_username: string) {
-    return null;
+  async findUserByUsername(username: string) {
+    const mongoUser = await userModel.findOne({username});
+    if (!mongoUser) return null;
+    return new User({
+      userId: mongoUser._id.toString(),
+      username: mongoUser.username,
+      email: mongoUser.email,
+      password: "",
+    });
   }
 
-  async findUserByEmail(_email: string) {
-    return null;
+  async findUserByEmail(email: string) {
+    const mongoUser = await userModel.findOne({email});
+    if (!mongoUser) return null;
+    return new User({
+      userId: mongoUser._id.toString(),
+      username: mongoUser.username,
+      email: mongoUser.email,
+      password: "",
+    });
   }
 
-  async findUserByEmailOrUsername(_email: string, _username: string) {
-    return null;
+  async findUserByEmailOrUsername(email: string, username: string) {
+    const mongoUser = await userModel.findOne({$or: [{username}, {email}]});
+    if (!mongoUser) return null;
+    return new User({
+      userId: mongoUser._id.toString(),
+      username: mongoUser.username,
+      email: mongoUser.email,
+      password: "",
+    });
   }
 }
